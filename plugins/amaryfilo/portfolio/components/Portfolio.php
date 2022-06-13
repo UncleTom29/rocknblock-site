5d72b3b94c428885e4be79b1d997c6ab947ce9a2
amaryfilo <laumiqv@gmail.com>
2022-06-14 02:59:24 +0300

    feat(portfolio): update component

--- plugins/amaryfilo/portfolio/components/Portfolio.php
+++ plugins/amaryfilo/portfolio/components/Portfolio.php
@@ -1,6 +1,7 @@
 <?php namespace Amaryfilo\Portfolio\Components;
 
 use Cms\Classes\ComponentBase;
+use Amaryfilo\Portfolio\Models\Portfolio as MPortfolio;
 
 class Portfolio extends ComponentBase
 {
@@ -15,5 +16,13 @@ class Portfolio extends ComponentBase
     public function defineProperties()
     {
         return [];
+    }
+
+    public function getPortfolioContracts() {
+        return MPortfolio::orderBy('id', 'desc')->where('type', 'contracts')->get();
+    }
+
+    public function getPortfolioWeb() {
+        return MPortfolio::orderBy('id', 'desc')->where('type', 'webMobile')->get();
     }
 }
